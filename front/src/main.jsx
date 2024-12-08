13762c999acf6889592160f6e4a0376dfbe1f726
ali mostafa2510 <es-aly.moustafa2027@alexu.edu.eg>
2024-12-08 21:07:45 +0200

    the backend and the router library

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+import { Link } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App></App>
+  
   </StrictMode>,
 )
